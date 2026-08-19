1749f5e5533b4d5d91cfb3d9e579587de027e19d
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2026-08-20 06:55:38 +0800

    1

---
+++ src/component/avator.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { UserIcon } from 'tdesign-icons-react';
+import { Avatar, Space } from 'tdesign-react';
+
+export default function BasicAvatar() {
+  return (
+    <Space>
+      <Avatar
+        icon={<UserIcon />}
+        style={{
+          marginRight: '40px',
+        }}
+      />
+      <Avatar
+        image="https://tdesign.gtimg.com/site/avatar.jpg"
+        hideOnLoadFailed={false}
+        style={{
+          marginRight: '40px',
+        }}
+      />
+      <Avatar
+        style={{
+          marginRight: '40px',
+        }}
+      >
+        W
+      </Avatar>
+    </Space>
+  );
+}
