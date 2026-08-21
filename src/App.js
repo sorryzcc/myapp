a35063bfd8ad01ec644bbe2a60f3781a90e4adc2
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2026-08-22 06:52:24 +0800

    avatar,inpu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './component/input.tsx';
+import Avator from './component/avatar.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Avator/>
+        <Input/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
